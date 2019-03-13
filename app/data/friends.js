34e21f2c7069bffa932dc4aa8a138766809923e6
Sergio Di Martino <sergio@webdevserg.io>
2019-03-13 08:59:26 -0700

    Adds routing, begins adding HTML

---
+++ app/data/friends.js
@@ -0,0 +1,14 @@
+var friends = [
+    {
+        name: "Joe",
+        photo: "google.com",
+        scores: ["1", "1", "1", "1", "1", "1", "1", "1"]
+    },
+    {
+        name: "Joe",
+        photo: "google.com",
+        scores: ["1", "1", "1", "1", "1", "1", "1", "1"]
+    }
+]
+
+module.exports = friends;

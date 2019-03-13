40ba54dc92a22d2268f493ed93393d153cc1f426
Sergio Di Martino <sergio@webdevserg.io>
2019-03-13 15:17:39 -0700

    Finishes adding survey questions and friends list

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,13 +1,33 @@
 var friends = [
     {
-        name: "Joe",
-        photo: "google.com",
-        scores: ["1", "1", "1", "1", "1", "1", "1", "1"]
+        name: "Joey",
+        photo: "https://upload.wikimedia.org/wikipedia/en/d/da/Matt_LeBlanc_as_Joey_Tribbiani.jpg",
+        scores: ["4", "1", "2", "3", "5", "5", "5", "2"]
     },
     {
-        name: "Joe",
-        photo: "google.com",
-        scores: ["1", "1", "1", "1", "1", "1", "1", "1"]
+        name: "Pheobe",
+        photo: "https://upload.wikimedia.org/wikipedia/en/f/f6/Friendsphoebe.jpg",
+        scores: ["5", "2", "4", "5", "1", "2", "3", "5"]
+    },
+    {
+        name: "Ross",
+        photo: "https://upload.wikimedia.org/wikipedia/en/6/6f/David_Schwimmer_as_Ross_Geller.jpg",
+        scores: ["1", "5", "4", "2", "2", "1", "3", "4"]
+    },
+    {
+        name: "Monica",
+        photo: "https://upload.wikimedia.org/wikipedia/en/d/d0/Courteney_Cox_as_Monica_Geller.jpg",
+        scores: ["5", "3", "5", "5", "1", "2", "4", "2"]
+    },
+    {
+        name: "Rachel",
+        photo: "https://en.wikipedia.org/wiki/File:Jennifer_Aniston_as_Rachel_Green.jpg",
+        scores: ["4", "2", "4", "3", "1", "2", "5", "3"]
+    },
+    {
+        name: "Chandler",
+        photo: "ghttps://upload.wikimedia.org/wikipedia/en/6/6c/Matthew_Perry_as_Chandler_Bing.jpg",
+        scores: ["5", "4", "4", "4", "4", "1", "3", "2"]
     }
 ]
 
